6432126941e565a6e9a835de35750d00900759aa
Henrique Moura <henriq8383@gmail.com>
2022-07-06 23:51:15 -0300

    lessons 7, 8, 9, 10 and 11
    Flyind and Bomber enemies added, animation included;
    Randomized spawn feature added (Flying enemies spawn between a particular range, and Bomber enemies spawn in one of three possible spawnpoints);
    Player shooting feature added;
    Enemy class created;
    Script sub-folders created.

---
+++ InfinityRunner/Assets/Scripts/Enemies/FlyingEnemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemy : Enemy
+{
+    private Rigidbody2D rig;
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rig.velocity = Vector2.left * speed;
+    }
+}
